330216d1dfeb03607c3047d2a7c3b5c6d85bc0f8
Dunya Kirkali <dunyakirkali@gmail.com>
2018-08-04 21:31:25 +0200

    Tweaks

--- Crawler/ViewController.swift
+++ Crawler/ViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import Cocoa
+import MapKit
 
 class ViewController: NSViewController {
 
     @IBOutlet weak var fromField: NSTextField!
     @IBOutlet weak var toField: NSTextField!
     @IBOutlet weak var searchButton: NSButton!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    fileprivate var searchCompleter = MKLocalSearchCompleter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        searchCompleter.delegate = self
+        mapView.delegate = self
         fromField.delegate = self
         toField.delegate = self
+        
         searchButton.action = #selector(ViewController.search)
+        
+        let request = MKDirectionsRequest()
+        request.source = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 52.3446243, longitude: 4.8002163), addressDictionary: nil))
+        request.destination = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 52.3626209, longitude: 4.8765708), addressDictionary: nil))
+        request.requestsAlternateRoutes = true
+        request.transportType = .walking
+        
+        let directions = MKDirections(request: request)
+        
+        directions.calculate { [unowned self] response, error in
+            guard let unwrappedResponse = response else { return }
+            
+            for route in unwrappedResponse.routes {
+                self.mapView.add(route.polyline)
+                self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+            }
+        }
     }
 
     override var representedObject: Any? {
@@ -29,14 +53,31 @@ class ViewController: NSViewController {
     }
 }
 
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(polyline: overlay as! MKPolyline)
+        renderer.strokeColor = .blue
+        return renderer
+    }
+}
+
+extension ViewController: MKLocalSearchCompleterDelegate {
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        if let result = completer.results.first {
+            print(result.title)
+        }
+    }
+}
+
 extension ViewController: NSTextFieldDelegate {
     override func controlTextDidChange(_ obj: Notification) {
         let field = obj.object as! NSTextField
+        searchCompleter.region = mapView.region
         switch field {
         case fromField:
-            print("Aa")
+            searchCompleter.queryFragment = fromField.stringValue
         case toField:
-            print("Bb")
+            searchCompleter.queryFragment = toField.stringValue
         default:
             print("~~")
         }
