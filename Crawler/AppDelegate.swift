33c8a939bdc41e7e9cb62cf995c8d6f2e0dc6671
Dunya Kirkali <dunyakirkali@gmail.com>
2018-08-04 13:37:56 +0200

    Initial commit

---
+++ Crawler/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Crawler
+//
+//  Created by Dunya Kirkali on 8/4/18.
+//  Copyright © 2018 Ahtung. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Crawler/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Crawler
+//
+//  Created by Dunya Kirkali on 8/4/18.
+//  Copyright © 2018 Ahtung. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
